1bb9e120d51f3d3c2724129a835c96f751c491af
Atheesh Thanaseelan <atheesh.k28@gmail.com>
2024-08-16 22:17:01 -0400

    pagination on gallery page

--- public/scripts/gallery.js
+++ public/scripts/gallery.js
@@ -1,5 +1,4 @@
-
-
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 
 // ---------------------- Pagination section ----------------------
@@ -7,12 +6,54 @@
 // On reaching bottom of page, add more data to that variable, and append
 
 // Optional: Top loading? Upon reaching top of page, reload previously displayed things? 
+// const picturesGrid = document.querySelector('.pictures-grid');
 
-console.log(data)
-function loadNewGallery(){
-  
+const categoryGrid = document.querySelector('.hoverEffect');
+async function loadImages(){
+    for (let index = 0; index < (data.length)-count; index++) {
+        if (count<2){
+            divisor = ((count%2)+1)
+            categoryGrid.style.gridTemplateColumns = Array(divisor+1).join(((100/divisor)+"% "))
+        }else{ categoryGrid.style.gridTemplateColumns = "50% 50%"}
+        
+        newHtml = `
+        <figure id="image-`+count+`" onclick="fullscreen(`+count+`)" class="picture-card-container">
+            <div class="picture-card" ><img src="`+data[count]["thumbnailLink"]+`" alt="image 1" referrerPolicy="no-referrer"></div>    
+            <figcaption>
+                <h1>`+data[count]["title"]+`</h1>
+                <small>`+data[count]["caption"]+`</small>
+                <a onclick="fullscreen(`+count+`)" href="#" class="more-button">Expand</a>
+            </figcaption>
+        </figure>`
+        categoryGrid.insertAdjacentHTML('beforeend',newHtml)
+        
+
+        $("#image-"+count).fadeOut(1)
+        $("#image-"+count).fadeIn()
+        count += 1;
+        await delay(500)
+    }
 }
-
+document.addEventListener('DOMContentLoaded', function() {
+    loadImages();
+});
+$(window).scroll(function() {
+    if($(window).scrollTop() == $(document).height() - $(window).height()) {
+        (async function(){
+            for (let index = 0; index < 4; index++) {
+                data.push({
+                    "title": "Two Cameras",
+                    "caption": "A picture of two cameras taken with one camera.",
+                    "type": "youtube_embed",
+                    "fullLink": "TxJtoBAa2w0",
+                    "thumbnailLink": "https://lh3.googleusercontent.com/fife/ALs6j_FmCcJjgO4XfABJeztorvhTi953xOzM0WISMT0lVya2Nmd-7HTiwANXn7TNb3SvM2TAGyoFuSItOZOSwvfD7i0fNE7dem8RvEv4QWR8Y1RxZ_dXmpJW0AzhAP4rAQt9V8wXm1mJkYHRZ5GanIvbYzRDf3smJSM8sFlchd_NIqU5qMH7vBlwV5kLATPDX4pVOnmhElff0t_me5Mf8T0Li2zA7VfYhgYsv5a7XbrojETYdicUPDHRI3gId6HwOXD_AUrDe59I6eh4X6mVPfmMWJ1ecBT6OzHEl_xjO0XX3UBxk1iMzjUyq-RGdfPklzixq-zq43XbfKkvHTNi3w7gSTnRxIaSgwW5LdEfIHuHbRaayzSZ4dbnlgqvsE2J2nc3Cm5AwqnLTPkbJvk2fW1HTLsNRzOM0XeZUhbkXNzjo_hPoUH240S-77rL5Z13iuOnAVEilHMU5oxaxyXBFPJwp0QxXkVEk9ydugky8tKkxccPm7TzEBhy7u7REcGey-ccdFT3HFdSbvq1M8JDt8MNDKvhtTa6jnqjacqQtblGx9d2worid9wXyD0hoNpht-a-FVAHrZEWkh0iNIGNPrWGK4vh5q3dJK0C80rGH1l3Z4i6cVBII_l-uM0CKwLOmkN46wf4a0ZoYBkJh9qgmEr6Lh-LRDP9lpuy173iY6O8HduXk22PRkiqJl8-pBOzi7SRcASFDRjjrrpWkmOpYe225J4x7tbmxyKPuulko1WtwME2vq2mgTDN0LPAlEKZ2apkc1jH3JeO-M6wS4-zCuKSyBvUZsqXJJN2d1WJFrcGDvJ_CCWAtFbtQ-j2ig5QJrwzqrWfy6uZck-wjy6Dugi9D5qeJ2QEqGs92LSCHYyHswLErSZRcFe7FKCRcanrA3rhtSYHFElFD0-iTKtbAEY78sygbAEt0hg1NPQpc26cWyScxrTNQnniU3LjB8WsIjRsdNMKhMBQxUKxOTy79DI7eKioBgl4X-SemBRV9KSPL2x2NeTH4obU11dy-LDpsLhWUFmzLmZBnBNAVF9i_SZHcIj1dogi62sCwDjl6bXa7ItFSCPHk_0840sMOelRGiZcJROhbMe9EXW_24Y__OWX08pm-IEETqQQEknnYoO5f--R6RAJPaBEuOSEFXO5R0Vy-cIIX54QCqIkchJI21lv_DF0boaKzPIPXCTNoW4ny1ZG_hbIHG1HP9cGQK69UPjUQuyDe-pLXJTbptcFmg2tkMyEm6EjciQRjb95SCExqjDLsDDFnPZ7COaJa9jnVgv_ye0CAFrGLx8SFga34ppNm_8r1q4j_1CgQlIdbBSBY4y8V6V-osj8gRfqmqrtMHkO7f9dy_erT46tDZ8slR6cl2LWd2l_YAzHHqX3uany_4lLR_cNQIkgGOGpr1jGFWB3o2KjNSxfDdigHF4KRqmBSxS5RNPn_pFiyeVCAdlB2-IjizE_bxI7niBy7PRVmOgc95lwavKaxldOQCyh3FzuDGJ415yvlgqYjlKwJVKUpHX9mBGI281ZKG8Pwj23iGWx8p8Eb3bETzU2B9za-claVml03UMu2Pio0PIKzyOR3TydpkwtqCvd1ADXZxB3CCCbqKkuenLBOgam5s4441lnbtQWLxlwT6gJvCu490Gm9guvrbwFCQyJ2gs=w1920-h940"
+                })
+            }   
+            
+            loadImages();
+        })();
+    }
+});
 
 // ---------------------- Fullscreen Loading section ----------------------
 // Load YT API lib
@@ -21,67 +62,51 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var fadeEffect = setInterval(function(){},1000);
 
 function closeFullscreen(){
-  console.log("hi")
-  clearInterval(fadeEffect);
-  fadeEffect = setInterval(function () {
-      var fadeTarget = document.getElementById("fullscreen-frame");
-      if (!fadeTarget.style.opacity) {
-          fadeTarget.style.opacity = 1;
-      }
-      if (fadeTarget.style.opacity > 0) {
-          fadeTarget.style.opacity = parseFloat(fadeTarget.style.opacity) - 0.05;
-      } else {
-          console.log("clear")
-          clearInterval(fadeEffect);
-      }
-  }, 25);
-  document.getElementById("fullscreen-frame").style.zIndex = -10000;
-
-  document.getElementById("fullscreen-content").remove()
-
+    (async function(){
+        $("#fullscreen-frame").fadeOut(400)
+        await delay(400)
+        document.getElementById("fullscreen-frame").style.zIndex = -10000;
+        document.getElementById("fullscreen-content").remove()        
+    })();
 }
 
 function fullscreen(contentId){
-  console.log(contentId)
 
-  // Loading a full screen video
-  document.getElementById("fullscreen-closebtn").insertAdjacentHTML('afterend','<video id="fullscreen-content" src="" controls></video>')
-  var fullscreenImg = document.getElementById("fullscreen-content");
-  fullscreenImg.src = "https://filesamples.com/samples/video/mp4/sample_1280x720.mp4"
+    (async function(){
+        console.log(contentId)
 
-
-  // Loading a full screen image
-  // document.getElementById("fullscreen-closebtn").insertAdjacentHTML('afterend','<img id="fullscreen-content" src="">')
-  // var fullscreenImg = document.getElementById("fullscreen-content");
-  // fullscreenImg.src = "https://lh3.googleusercontent.com/fife/ALs6j_FmCcJjgO4XfABJeztorvhTi953xOzM0WISMT0lVya2Nmd-7HTiwANXn7TNb3SvM2TAGyoFuSItOZOSwvfD7i0fNE7dem8RvEv4QWR8Y1RxZ_dXmpJW0AzhAP4rAQt9V8wXm1mJkYHRZ5GanIvbYzRDf3smJSM8sFlchd_NIqU5qMH7vBlwV5kLATPDX4pVOnmhElff0t_me5Mf8T0Li2zA7VfYhgYsv5a7XbrojETYdicUPDHRI3gId6HwOXD_AUrDe59I6eh4X6mVPfmMWJ1ecBT6OzHEl_xjO0XX3UBxk1iMzjUyq-RGdfPklzixq-zq43XbfKkvHTNi3w7gSTnRxIaSgwW5LdEfIHuHbRaayzSZ4dbnlgqvsE2J2nc3Cm5AwqnLTPkbJvk2fW1HTLsNRzOM0XeZUhbkXNzjo_hPoUH240S-77rL5Z13iuOnAVEilHMU5oxaxyXBFPJwp0QxXkVEk9ydugky8tKkxccPm7TzEBhy7u7REcGey-ccdFT3HFdSbvq1M8JDt8MNDKvhtTa6jnqjacqQtblGx9d2worid9wXyD0hoNpht-a-FVAHrZEWkh0iNIGNPrWGK4vh5q3dJK0C80rGH1l3Z4i6cVBII_l-uM0CKwLOmkN46wf4a0ZoYBkJh9qgmEr6Lh-LRDP9lpuy173iY6O8HduXk22PRkiqJl8-pBOzi7SRcASFDRjjrrpWkmOpYe225J4x7tbmxyKPuulko1WtwME2vq2mgTDN0LPAlEKZ2apkc1jH3JeO-M6wS4-zCuKSyBvUZsqXJJN2d1WJFrcGDvJ_CCWAtFbtQ-j2ig5QJrwzqrWfy6uZck-wjy6Dugi9D5qeJ2QEqGs92LSCHYyHswLErSZRcFe7FKCRcanrA3rhtSYHFElFD0-iTKtbAEY78sygbAEt0hg1NPQpc26cWyScxrTNQnniU3LjB8WsIjRsdNMKhMBQxUKxOTy79DI7eKioBgl4X-SemBRV9KSPL2x2NeTH4obU11dy-LDpsLhWUFmzLmZBnBNAVF9i_SZHcIj1dogi62sCwDjl6bXa7ItFSCPHk_0840sMOelRGiZcJROhbMe9EXW_24Y__OWX08pm-IEETqQQEknnYoO5f--R6RAJPaBEuOSEFXO5R0Vy-cIIX54QCqIkchJI21lv_DF0boaKzPIPXCTNoW4ny1ZG_hbIHG1HP9cGQK69UPjUQuyDe-pLXJTbptcFmg2tkMyEm6EjciQRjb95SCExqjDLsDDFnPZ7COaJa9jnVgv_ye0CAFrGLx8SFga34ppNm_8r1q4j_1CgQlIdbBSBY4y8V6V-osj8gRfqmqrtMHkO7f9dy_erT46tDZ8slR6cl2LWd2l_YAzHHqX3uany_4lLR_cNQIkgGOGpr1jGFWB3o2KjNSxfDdigHF4KRqmBSxS5RNPn_pFiyeVCAdlB2-IjizE_bxI7niBy7PRVmOgc95lwavKaxldOQCyh3FzuDGJ415yvlgqYjlKwJVKUpHX9mBGI281ZKG8Pwj23iGWx8p8Eb3bETzU2B9za-claVml03UMu2Pio0PIKzyOR3TydpkwtqCvd1ADXZxB3CCCbqKkuenLBOgam5s4441lnbtQWLxlwT6gJvCu490Gm9guvrbwFCQyJ2gs=w1920-h940"
-
-  // Loading a yt video
-  // document.getElementById("fullscreen-closebtn").insertAdjacentHTML('afterend','<div class="popupMedia" id="fullscreen-content"></div>')
-  // var player2 = new YT.Player('fullscreen-content', {
-  //     videoId: 'TxJtoBAa2w0',
-  //     playerVars: {
-  //         'playsinline': 1
-  //     },
-  // })
+        // Loading a full screen video
+        // document.getElementById("fullscreen-closebtn").insertAdjacentHTML('afterend','<video id="fullscreen-content" src="" controls></video>')
+        // var fullscreenImg = document.getElementById("fullscreen-content");
+        // fullscreenImg.src = "https://filesamples.com/samples/video/mp4/sample_1280x720.mp4"
 
 
-  document.getElementById("fullscreen-frame").style.zIndex = 10000;
-  clearInterval(fadeEffect);
-  fadeEffect = setInterval(function () {
-      var fadeTarget = document.getElementById("fullscreen-frame");
-      if (!fadeTarget.style.opacity) {
-          fadeTarget.style.opacity = 0;
-      }
-      if (fadeTarget.style.opacity < 1) {
-          fadeTarget.style.opacity = parseFloat(fadeTarget.style.opacity) + 0.05;
-      } else {
-          console.log("clear")
-          clearInterval(fadeEffect);
-      }
-  }, 25);
+        // Loading a full screen image
+        if(data[contentId]["type"]=="image_link"){
+            document.getElementById("fullscreen-closebtn").insertAdjacentHTML('afterend','<img id="fullscreen-content" src="">')
+            var fullscreenImg = document.getElementById("fullscreen-content");
+            fullscreenImg.src = data[contentId]["fullLink"]
+        }
+
+        // Loading a yt video
+        if(data[contentId]["type"]=="youtube_embed"){
+            document.getElementById("fullscreen-closebtn").insertAdjacentHTML('afterend','<div class="popupMedia" id="fullscreen-content"></div>')
+            var player2 = new YT.Player('fullscreen-content', {
+                videoId: data[contentId]["fullLink"],
+                playerVars: {
+                    'playsinline': 1
+                },
+            })
+        }
+        document.getElementById("fullscreen-frame").style.zIndex = 10000;
+        var fadeTarget = document.getElementById("fullscreen-frame");
+        fadeTarget.style.opacity = 1;
+        $("#fullscreen-frame").fadeOut(1)
+        $("#fullscreen-frame").fadeIn(400)
+        await delay(400)
+    })();
 
 
 }
